2ac415eac4a59808cfc4a54097451ec0b706341e
Saarine-E <sienihemmo@gmail.com>
2025-04-24 20:35:29 +0300

    Reordered endpoints to be more readable in the OpenAPI docs

--- app/routers/sensors.py
+++ app/routers/sensors.py
@@ -10,6 +10,10 @@ router = APIRouter(prefix="/sensors", tags=["Sensors"])
 def Get_All_Sensors(errorState: bool = None, session: Session = Depends(get_session)):
     return sensors_crud.Get_All_Sensors(session, errorState)
 
+@router.post("/new", response_model=Sensor, status_code=status.HTTP_201_CREATED)
+def Add_Sensor(sensorIn: SensorIn, session: Session = Depends(get_session)):
+    return sensors_crud.Add_Sensor(session, sensorIn)
+
 @router.get("/errors", response_model=list[ErrorHistory], status_code=status.HTTP_200_OK)
 def Get_Error_History(session: Session = Depends(get_session)):
     print("asd")
@@ -19,14 +23,10 @@ def Get_Error_History(session: Session = Depends(get_session)):
 def Get_Sensor(sensorId: int, measurementCount: int = 10, startDateTime: str = None, endDateTime: str = None, session: Session = Depends(get_session)):
     return sensors_crud.Get_Sensor(session, sensorId, measurementCount, startDateTime, endDateTime)
 
-@router.post("/new", response_model=Sensor, status_code=status.HTTP_201_CREATED)
-def Add_Sensor(sensorIn: SensorIn, session: Session = Depends(get_session)):
-    return sensors_crud.Add_Sensor(session, sensorIn)
+@router.patch("/{sensorId}", response_model=ErrorHistory, status_code=status.HTTP_200_OK)
+def Change_Sensor_Error_State(sensorId: int, newErrorState: bool = True, session: Session = Depends(get_session)):
+    return sensors_crud.Change_Sensor_Error_State(session, sensorId, newErrorState)
 
 @router.get("/{sensorId}/errors", response_model=list[ErrorHistory])
 def Get_Sensor_Error_History(sensorId: int, session: Session = Depends(get_session)):
     return sensors_crud.Get_Sensor_Error_History(session, sensorId)
-
-@router.patch("/{sensorId}", response_model=ErrorHistory, status_code=status.HTTP_200_OK)
-def Change_Sensor_Error_State(sensorId: int, newErrorState: bool = True, session: Session = Depends(get_session)):
-    return sensors_crud.Change_Sensor_Error_State(session, sensorId, newErrorState)
